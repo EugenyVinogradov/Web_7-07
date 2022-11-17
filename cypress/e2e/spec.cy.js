e9258c0c1c755a98ef8b9f5da3147e8f2c65e209
EugenyVinogradov <vinogradov_e_v@rambler.ru>
2022-11-17 16:35:25 +0300

    Task 1

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,51 +1,70 @@
-// const { describe } = require("mocha");
+const seats = require("../fixtures/seats.json");
 const { selectors } = require("../fixtures/selectors");
 const { admin } = require("../fixtures/usrers");
 
-
-
-
 it("correct display of the main page", () => {
-  cy.visit('/');
-  cy.get(selectors.daysToolbar).should('have.length', 7);
-  cy.get(selectors.moviesTitle).should('be.visible');
-})
+  cy.visit("/");
+  cy.get(selectors.daysToolbar).should("have.length", 7);
+  cy.get(selectors.moviesTitle).should("be.visible");
+});
 
-
-describe('login admin', () => {
-
-  beforeEach(function() {          
+describe("login admin", () => {
+  beforeEach(function () {
     cy.visit(admin.URL);
   });
 
-  it('login admin happy path', () => {
+  it("login admin happy path", () => {
     cy.login(admin.login, admin.password);
-    cy.contains('Администраторррская');
-  })
+    cy.contains("Администраторррская");
+  });
 
-  it('login admin sad path wrong password', () => {
+  it("login admin sad path wrong password", () => {
     cy.login(admin.login, admin.wrongPassword);
-    cy.contains('Ошибка авторизации!');
-  })
+    cy.contains("Ошибка авторизации!");
+  });
 
-  it('login admin sad path wrong login', () => {
+  it("login admin sad path wrong login", () => {
     cy.login(admin.wrongLogin, admin.password);
-    cy.contains('Ошибка авторизации!');
-  })
+    cy.contains("Ошибка авторизации!");
+  });
+});
 
-})
-
-
-it.only('booking three tickets on next day', () => {
+it.only("booking three tickets on next day", () => {
   cy.visit(admin.URL);
   cy.login(admin.login, admin.password);
-  const hallsArray = cy.get(selectors.hallsArray).invoke('text');
-  cy.log(hallsArray);    
-  // console.log(movieFirst);
-  // cy.visit('/');
-  // cy.get(selectors.nextDay).click();
-  // cy.contains(movieFirst);
-
-
-
-})
+  const firstHall = cy
+    .xpath(selectors.firstHall)
+    .then(($el) => $el.textContent)
+    .should("have.text", "data.newHall");
+  cy.xpath(selectors.firstHall)
+    .invoke("text")
+    .then((hallName) => {
+      cy.visit("/");
+      cy.get(selectors.nextDay).click();
+      cy.get(selectors.hallsInTheater).last().should("have.text", hallName);
+      cy.contains(hallName)
+        .parent()
+        .children(selectors.seance)
+        .children(selectors.seanceTime)
+        .click();
+      seats.forEach((seats) => {
+        let selector =
+          selectors.seatsPart1 +
+          `${seats.row}` +
+          selectors.seatsPart2 +
+          `${seats.seat}` +
+          selectors.seatsPart3;
+        cy.get(selector).click();
+      });
+      cy.get(selectors.bookingButton).click();
+      cy.get(selectors.bookingTitle).should("have.text", "Вы выбрали билеты:");
+      cy.get(selectors.bookingSeatsAndRows).should(
+        "have.text",
+        "Ряд/Место: 5/4, 5/5, 5/6"
+      );
+      cy.get(selectors.bookingHall).should("have.text", `В зале: ${hallName}`);
+      cy.contains("Получить код бронирования")
+        .should("be.visible")
+        .and("not.be.disabled");
+    });
+});
--- cypress/fixtures/selectors.js
+++ cypress/fixtures/selectors.js
@@ -5,7 +5,17 @@ const selectors = {
   "password": ".login__input[type='password']",
   "authButton": "[type='submit']",
   "nextDay": ".page-nav__day:nth-child(2)",
-  "hallsArray": "#hall-control > div > ul > li",
+  "firstHall": "//*[@id='hall-control']/div/ul/li[1]/text()",
+  "hallsInTheater": ".movie-seances__hall-title",
+  "seance": ".movie-seances__list",
+  "seanceTime": ".movie-seances__time-block",
+  "seatsPart1": "div.buying-scheme__wrapper > div:nth-child(",
+  "seatsPart2": ") > span:nth-child(",
+  "seatsPart3": ")",
+  "bookingButton": ".acceptin-button",
+  "bookingTitle": "h2.ticket__check-title",
+  "bookingSeatsAndRows": ".ticket__info:nth-child(2)",
+  "bookingHall": ".ticket__info:nth-child(3)"
 
 }
 
